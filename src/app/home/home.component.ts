c33770510df18e9653e1bb66bbfa6350e5a8616c
Rodgersouko <gabrielaouko@gmail.com>
2022-03-23 15:27:40 +0300

    added description of the homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [NgbCarouselConfig]
 })
 export class HomeComponent implements OnInit {
+  showNavigationArrows = false;
+  showNavigationIndicators = false;
+  images = [1055, 194, 368].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
-  constructor() { }
+  constructor(config: NgbCarouselConfig) {
+    config.showNavigationArrows = true;
+    config.showNavigationIndicators = true;
+   }
 
   ngOnInit(): void {
   }
 
 }
+
+// import { Component } from '@angular/core';
+
+// @Component({selector: 'ngbd-carousel-basic', templateUrl: './carousel-basic.html'})
+// export class NgbdCarouselBasic {
+//   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+// }
