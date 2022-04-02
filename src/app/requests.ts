350a488677cd93ac17b40a7cba95f44304aceb7e
mnubi <rodger7kamau@gmail.com>
2022-04-03 00:36:15 +0300

    created class for the product to be posted

---
+++ src/app/requests.ts
@@ -0,0 +1,9 @@
+export class Requests {
+    constructor(public Names: string, public Descriptions : number, public Prices : string){
+    }
+}
+
+export class Cake {
+    constructor(public Name: string, public Description : string, public Price : number){
+    }
+}
