8455bb8269a9ca782a917515939390f98d7c0d25
mnubi <rodger7kamau@gmail.com>
2022-06-28 09:11:52 +0300

    provided classes to add items and append in product catalogue

--- src/app/admins/admins.component.ts
+++ src/app/admins/admins.component.ts
@@ -9,13 +9,47 @@ import { Cake, Requests } from '../requests';
 export class AdminsComponent implements OnInit {
 
   cakes: Cake[] = [
-    new Cake('Martin Luther Jr', 'hghgags', 6450),
+    new Cake('Martin Luther Jr', 'hghgags', 6450,
+    ),
     new Cake('Maya Angelou', 'life best vanilla cake', 5235),
     new Cake('Lupita Nyongo', 'Actor', 18760),
+    new Cake('Mother Teressa', 'Nun', 23845),
+    new Cake('Lupita Nyongo', 'Actor', 18760),
     new Cake('Mother Teressa', 'Nun', 23845)
-  ]
+  ];
+
+  addNewCake(cake: any) {
+    let cakeLength = this.cakes.length;
+    cake.id = cakeLength + 1;
+    this.cakes.push(cake)
+  }
+
+  constructor() { }
 
   ngOnInit(): void {
+
   }
+
+
+  displatyle = "none";
+
+  openPopu() {
+    this.displatyle = "block";
+  }
+
+  closePopu() {
+    this.displatyle = "none";
+  }
+
+  dispstyle = "none";
+
+  openPop() {
+    this.dispstyle = "block";
+  }
+
+  closePop() {
+    this.dispstyle = "none";
+  }
+
 
 }
